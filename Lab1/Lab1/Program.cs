a56845f9ab41c046d16f195d3c36a970d0ed3a62
Stanisław Staniewski <sstaniewski@edu.cdv.pl>
2025-10-11 16:18:12 +0200

    Lab1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Example 1
 
-Console.WriteLine("Hello, World!");
+const int requiredAge = 18;
+const string accessDenied = "Musisz mieć 18 lat.";
+const string accessAllowed = "Witamy w naszym sklepie";
+
+int age = 19;
+
+do
+{
+    Console.WriteLine("Podaj swój wiek: ");
+
+    string input = Console.ReadLine();
+
+    bool success = int.TryParse(input, out age);
+
+    if (!success)
+    {
+        Console.WriteLine("Podaj poprawną wartość!");
+    }
+    else
+    {
+        if (age < requiredAge)
+        {
+            Console.WriteLine(accessDenied);
+        }
+        else
+        {
+            Console.WriteLine(accessAllowed);
+        }
+    }
+} while (age < requiredAge);
+
+// Example 2
+
+// var names = new[] { "Artur", "Alicja", "Michał", "Gosia" };
+//
+// for (int i = 0; i < names.Length; i++)
+// {
+//     Console.WriteLine(names[i]);
+// }
+//
+// foreach (var name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// DODANE – prosty wybór bez ponownego pytania o wiek
+
+Console.WriteLine("Proszę wybrać: alkohol lub bron");
+string wybor = Console.ReadLine();
+
+if (wybor == "alkohol")
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Możesz kupić alkohol.");
+    }
+    else
+    {
+        Console.WriteLine("Nie możesz kupić alkoholu. Musisz mieć co najmniej 18 lat.");
+    }
+}
+else if (wybor == "bron")
+{
+    if (age >= 21)
+    {
+        Console.WriteLine("Możesz kupić broń.");
+    }
+    else
+    {
+        Console.WriteLine("Nie możesz kupić broni. Musisz mieć co najmniej 21 lat.");
+    }
+}
+else
+{
+    Console.WriteLine("Niepoprawny wybór. Proszę wybrać 'alkohol' lub 'bron'.");
+}
