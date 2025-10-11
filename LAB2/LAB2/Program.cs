6be6eb9cee0cb5af58eaacc3cb75331dfc48c2b1
Stanisław Staniewski <sstaniewski@edu.cdv.pl>
2025-10-11 17:08:15 +0200

    Lab1

--- LAB2/LAB2/Program.cs
+++ LAB2/LAB2/Program.cs
@@ -1,3 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+//////////////////////////////Zadanie 5//////////////////////////////////////////////
+/*
+class KontoBankowe
+{
+    private double saldo;
+
+    public void Wplata(double kwota)
+    {
+        saldo += kwota;
+        Console.WriteLine("Wpłata: " + kwota);
+    }
+
+    public double PobierzSaldo()
+    {
+        return saldo;
+    }
+    public void Wyplata(double kwota)
+    {
+        if (kwota <= saldo)
+        {
+            saldo -= kwota;
+            Console.WriteLine("Wypłata udana: " + kwota);
+        }
+        else
+        {
+            Console.WriteLine("Za mało środków na koncie.");
+        }
+    }
+}
+*/
+//////////////////////////////Zadanie 6//////////////////////////////////////////////
+class Zwierze
+{
+    public void Jedz()
+    {
+        Console.WriteLine("Zwierzę je");
+    }
+}
+
+class Pies : Zwierze
+{
+    public void Szczekaj()
+    {
+        Console.WriteLine("Hau hau!");
+    }
+}
+
+class Kot : Zwierze
+{
+    public void Miaucz()
+    {
+        Console.WriteLine("Miau!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Test Zadanie 5
+        var konto = new KontoBankowe();
+        konto.Wplata(100);
+        konto.Wyplata(50);   // OK
+        konto.Wyplata(100);  // Za mało środków
+        Console.WriteLine("Saldo: " + konto.PobierzSaldo());
+
+        Console.WriteLine();
+
+        // Test Zadanie 6
+        var pies = new Pies();
+        pies.Jedz();
+        pies.Szczekaj();
+
+        var kot = new Kot();
+        kot.Jedz();
+        kot.Miaucz();
+
+        Console.WriteLine("\nKoniec. Naciśnij Enter...");
+        Console.ReadLine();
+    }
+}
